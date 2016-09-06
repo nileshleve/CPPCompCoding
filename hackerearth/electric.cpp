a63ca50a463186b9cfc7cdff978b170d60ea53ce
nileshleve <nilesh.leve04@gmail.com>
2016-09-06 20:22:07 +0530

    update

--- hackerearth/electric.cpp
+++ hackerearth/electric.cpp
@@ -16,15 +16,21 @@ int main(int argc, char const *argv[])
 		s.insert(a[i]);
 	}	
 	distinct = s.size();
+	map<int, string> map1;
+	int order = 0;
 	long long int ans = 0;
 	long long int count = 0;
 	set<long long int> notin;
-	for(long long int i = 0; i < n; i++){
+	for(long long int i = 0; i < n-1; i++){
 		set<long long int> newset;
 		if(notin.find(a[i]) == notin.end()){
 			for(long long int j = i+1; j < n; j++){
 			
 				newset.insert(a[j]);
+				stringstream ss;
+				ss<<i<<" "<<j;
+				string result = ss.str();
+				map1[order++] = result;
 			}
 			ans += newset.size();
 			notin.insert(a[i]);
@@ -33,6 +39,10 @@ int main(int argc, char const *argv[])
 		
 	}
 	cout<<ans<<endl;
+	// map<int, string>::iterator itmap;
+	// for(itmap = map1.begin(); itmap != map1.end(); itmap++){
+	// 	cout<<itmap->first<<"===>"<<itmap->second<<endl;
+	// }
 
 	return 0;
 }
