95c3c78039a5a18e39838f4675c6b84c652970f7
nileshleve <nilesh.leve04@gmail.com>
2017-04-18 21:16:21 +0530

    Gate  output question doubt

---
+++ gateTest.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std; 
+void fun2(int n);
+void fun1(int n){
+ 	if(n == 0) return;
+	printf("%d", n);
+	fun2(n-2);
+	printf("%d", n);
+}
+
+
+void fun2(int n){
+ 	if(n == 0) return;
+	printf("%d", n);
+	fun1(++n);
+	printf("%d", n);
+}
+
+int main(){
+	fun1(5);
+	return 0;
+}
